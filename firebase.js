45ebc5c4b7625014828f67ebda572eac69fa9319
nitika-0709 <148306870+nitika-0709@users.noreply.github.com>
2025-12-16 23:54:02 +0530

    Add Firebase configuration and initialization script

---
+++ firebase.js
@@ -0,0 +1,16 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyAceQf7DxVI9d_-HnVchw4tmiBCSy2P3NI",
+    authDomain: "shree-chakrdhar-booking-ed24a.firebaseapp.com",
+    projectId: "shree-chakrdhar-booking-ed24a",
+    storageBucket: "shree-chakrdhar-booking-ed24a.firebasestorage.app",
+    messagingSenderId: "12806299260",
+    appId: "1:12806299260:web:eeacbd29ba01bcd216f3f9"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+</script>
